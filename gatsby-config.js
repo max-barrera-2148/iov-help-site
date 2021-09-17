d7733ee8a95bbe2e4fc55a28625851d54b86ff9b
Max B <m.a.barrera6248@gmail.com>
2021-09-16 23:08:53 -0500

    removed netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-netlify-cms`,
+   
     // {
     //   resolve: `gatsby-plugin-google-analytics`,
     //   options: {
